7c5b210a0316442b3c98272625482bd22dc73fa9
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-03-22 16:23:37 -0700

    update path for reviewer complete

--- __tests__/reviewer.test.js
+++ __tests__/reviewer.test.js
@@ -29,9 +29,31 @@ describe('book-store routes', () => {
   });
     
   it('get a reviewer by id', async () => {
-    const reviewer = await Reviewer.insert({ name: 'Sally', company: 'The Cool Company' });
+    const reviewer = await Reviewer.insert({ name: 'Sally', company: 'The Cool Company'
+    // , reviews: [{
+    //   id: expect.any(String),
+    //   rating: 5,
+    //   review: 'Very good book!',
+    //   book_id: 1,
+    //   book_title: 'The Road'
+      // }] 
+    });
     const res = await request(app).get(`/api/v1/reviewers/${reviewer.id}`);
       
     expect(res.body).toEqual(reviewer);
   });
+    
+  it('updates a reviewer by id', async () => {
+    const reviewer = await Reviewer.insert({ name: 'Sally', company: 'The Cool Company' });
+     
+    const res = await request(app)
+      .patch(`/api/v1/reviewers/${reviewer.id}`)
+      .send({ name: 'Maisie' });
+      
+    const expected = { id: reviewer.id, name: 'Maisie', company: 'The Cool Company' };
+
+    expect(res.body).toEqual(expected);
+    expect(await Reviewer.findReviewerById(reviewer.id)).toEqual(expected);
+  });
+    
 });
